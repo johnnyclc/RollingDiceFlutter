9c1fff607010a5be3e36ac054c38568dc7e4bd52
Johnny <1250856+JohnnyChien@users.noreply.github.com>
2019-06-19 23:41:27 -0700

    Completed the rolling dice app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,18 +8,50 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Rolling Dice'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: RollingDice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class RollingDice extends StatefulWidget {
+  @override
+  _RollingDiceState createState() => _RollingDiceState();
+}
+
+class _RollingDiceState extends State<RollingDice> {
+  int _dice1 = 1;
+  int _dice2 = 2;
+
+  void _roll() {
+    setState(() {
+      _dice1 = Random().nextInt(6) + 1;
+      _dice2 = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$_dice1.png'),
+              onPressed: () => _roll(),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$_dice2.png'),
+              onPressed: () => _roll(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
